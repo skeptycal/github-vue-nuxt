d1ac6bef0952419b2f0a813b86ca1e5e1ac447f3
skeptycal <skeptycal@gmail.com>
2019-07-02 07:02:31 -0500

    formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,27 +5,30 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'eslint:recommended',
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended"
   ],
   // required to lint *.vue files
-  plugins: ['vue'],
+  plugins: ["vue"],
   root: true,
   // add your custom rules here
   rules: {
-    'import/no-unresolved': 0,
-    'import/no-unassigned-import': 0,
-    semi: ['error', 'never'],
-    'vue/max-attributes-per-line': 'off',
-    'no-console': 'off',
-    'prettier/prettier': ['error', {
-      semi: false
-    }]
+    "import/no-unresolved": 0,
+    "import/no-unassigned-import": 0,
+    semi: ["error", "never"],
+    "vue/max-attributes-per-line": "off",
+    "no-console": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false
+      }
+    ]
   }
-}
+};
