266685f34c16c75a42589d38832f7d36f691b67d
skeptycal <skeptycal@gmail.com>
2019-07-02 07:02:32 -0500

    formatting

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,3 @@
-<style>
-.footer {
-  padding-left: 30px;
-}
-.titlebar-icon {
-  margin-left: 50px;
-}
-</style>
-
 <template>
   <v-app dark>
     <v-navigation-drawer
@@ -25,31 +16,26 @@
           exact
         >
           <v-list-tile-action>
-            <v-icon v-html="item.icon"/>
+            <v-icon v-html="item.icon" />
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"/>
+            <v-list-tile-title v-text="item.title" />
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer"/>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
 
-      <i class="titlebar-icon fa fa-github"/><v-toolbar-title v-text="title"/>
-      <v-spacer/>
+      <i class="titlebar-icon fa fa-github" /><v-toolbar-title v-text="title" />
+      <v-spacer />
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer :right="right" v-model="rightDrawer" temporary fixed>
       <v-list>
         <v-list-tile @click.native="right = !right">
           <v-list-tile-action>
@@ -65,23 +51,32 @@
   </v-app>
 </template>
 
-<script>
-import "font-awesome/css/font-awesome.css"
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        { icon: "search", title: "Search", to: "/" },
-        { icon: "list", title: "Browse", to: "/repos" }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "Github Preview SPA"
-    }
+<style>
+  .footer {
+    padding-left: 30px;
   }
-}
+  .titlebar-icon {
+    margin-left: 50px;
+  }
+</style>
+
+<script>
+  import "font-awesome/css/font-awesome.css";
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        items: [
+          { icon: "search", title: "Search", to: "/" },
+          { icon: "list", title: "Browse", to: "/repos" }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: "Github Preview SPA"
+      };
+    }
+  };
 </script>
