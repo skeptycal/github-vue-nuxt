a9c36f8df16c153eb8ee76671f0d83b31c81de3e
skeptycal <skeptycal@gmail.com>
2019-07-04 06:20:58 -0700

    Prettier formatting

--- pages/repos/_id.vue
+++ pages/repos/_id.vue
@@ -5,31 +5,29 @@
 </style>
 
 <template>
-  <v-container id="container"
-               fill-height>
-    <v-layout align-center
-              justify-center>
-      <repo :repo-information="repoInformation"/>
+  <v-container id="container" fill-height>
+    <v-layout align-center justify-center>
+      <repo :repo-information="repoInformation" />
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapState } from "vuex"
-import Repo from "~/components/repo"
+import { mapState } from "vuex";
+import Repo from "~/components/repo";
 export default {
   components: {
     Repo
   },
   head: { title: "Repo Information" },
   validate({ params }) {
-    return /^\d+$/.test(params.id)
+    return /^\d+$/.test(params.id);
   },
   async fetch({ store, params }) {
-    await store.dispatch("loadRepositoryInformationFromAPI", params.id)
+    await store.dispatch("loadRepositoryInformationFromAPI", params.id);
   },
   computed: {
     ...mapState(["repoInformation"])
   }
-}
+};
 </script>
